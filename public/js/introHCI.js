2287f7eaf5a44cddace424a0d557ebcaf5009cd8
vagrant <vagrant@precise32.(none)>
2014-02-13 18:51:54 +0000

    ajax lab first try

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -21,13 +21,35 @@ function addProjectDetails(e) {
 	// Prevent following the link
 	e.preventDefault();
 
+
 	// Get the div ID, e.g., "project3"
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	$.get("/project/" + idNumber, addProject);
+
 	console.log("User clicked on project " + idNumber);
+
+	$(".details" + ".project" + "idNumber")
+	$("div.details").html("<p>blahhh</p>");
 }
+
+function addProject(result){
+	console.log(result); 
+	var projectHTML = '<a href="#" class="detailsImage">' +
+		'<img src="' + result['image'] + '"class="img">' +
+		'<p>' + result['title'] + '</p>' +
+		'<p><small>' + result['date'] + '</small></p></a>';
+
+		$("#project-container").html(projectHTML);
+		$("#project-description").html(result)['summary'];
+
+}
+
+
+
+
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
